f48c97af6b3a16595c0c0e2233c5dc46291ab36f
arjunpkulkarni <arjunpkulkarni@gmail.com>
2025-08-17 15:26:45 -0400

    push

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Typography,
   Container,
   Grid,
+  Box,
 } from "@mui/material";
 import ChallengeForm from "./components/ChallengeForm";
 import CampusForm from "./components/CampusForm";
@@ -15,26 +16,33 @@ function App() {
   return (
     <>
       <CssBaseline />
-      <AppBar position="static" elevation={2}>
-        <Toolbar>
-          <Typography variant="h6" component="h1">
-            Admin Dashboard
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6}>
-            <CampusForm />
+      <Box sx={{ bgcolor: "grey.50", minHeight: "100vh" }}>
+        <AppBar position="static" elevation={2}>
+          <Toolbar>
+            <Typography variant="h6" component="h1">
+              Admin Dashboard
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Container
+          component="main"
+          maxWidth={false}
+          disableGutters
+          sx={{ py: 4, px: { xs: 2, sm: 3, md: 4 } }}
+        >
+          <Grid container spacing={4} alignItems="stretch">
+            <Grid item xs={12} md={6}>
+              <CampusForm />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <UserForm />
+            </Grid>
+            <Grid item xs={12}>
+              <ChallengeForm />
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={6}>
-            <UserForm />
-          </Grid>
-          <Grid item xs={12}>
-            <ChallengeForm />
-          </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </Box>
     </>
   );
 }
